e3d113bccea6b15c3b7922af7e62d04017142f74
Bhagyeshpro <bhagyeshpaunikar2001@gmail.com>
2021-12-04 21:42:01 +0530

    First useEffect()

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [name, setName] = useState("");
+
+  //On Every Render
+  useEffect(() => {
+    console.log("I re-rendered");
+  });
+
+  //On First Render/Mount - componentDidMount
+  useEffect(() => {
+    console.log("Component Is Mounted");
+  }, []);
+
   return (
     <div className="App">
       <h1>React Mastery</h1>
+      <h2>UseEffect</h2>
+      <h3>Your Window Width is {windowWidth}</h3>
+      <input
+        type="text"
+        placeholder="Enter Your Name"
+        onChange={(e) => setName(e.target.value)}
+      />
     </div>
   );
 }

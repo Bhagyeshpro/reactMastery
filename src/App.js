d534ce60858c9a8cabd5579dfea5b03702231daf
Bhagyeshpro <bhagyeshpaunikar2001@gmail.com>
2021-12-22 13:00:48 -0800

    Custom hook done!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
+import {useEffect, useState } from "react"
+import useRandomJoke from "./hooks/useRandomJoke";
 
 function App() {
+  const joke = useRandomJoke()
+
   return (
     <div className="App">
-      <h1>React Mastery</h1>
+      <h1>Joke Generator</h1>
+      <h2>{joke}</h2>
+      <button >Generate Joke</button>
     </div>
   );
 }

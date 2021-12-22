16ccc6abec7ff13fcef15874ecac0dd1642d0a8f
Bhagyeshpro <bhagyeshpaunikar2001@gmail.com>
2021-12-22 13:41:02 -0800

    Ref and custom hooks Done!

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+import { useRef, useState } from "react";
 import "./App.css";
 import useRandomJoke from "./hooks/useRandomJoke";
 
 function App() {
-  const joke = useRandomJoke('Toy', 'Bhat')
+  const firstNameRef = useRef(null)
+  const lastNameRef = useRef(null)
+  
+  const [firstName, setFirstName] = useState('Captain')
+  const [lastName, setLastName] = useState('America')
+  
+  const joke = useRandomJoke(firstName, lastName)
+
+  const generateJoke = (e) => {
+    e.preventDefault()
+    setFirstName(firstNameRef.current.value)
+    setLastName(lastNameRef.current.value)
+  }
 
   return (
     <div className="App">
       <h1>Joke Generator</h1>
       <h2>{joke}</h2>
-      <button >Generate Joke</button>
+      <form>
+        <input placeholder="firstName" ref={firstNameRef} />
+        <input placeholder="lastName" ref={lastNameRef} />
+        <button onClick={generateJoke} >Generate Joke</button>
+      </form>
     </div>
   );
 }
